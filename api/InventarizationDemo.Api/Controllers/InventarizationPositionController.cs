a4b493366f76fddd5df3b97b9dadd27f9de812f4
Bartosz Żołyński <bartosz.zolynski@compilon.com>
2022-05-11 14:24:56 +0200

    Add create inventarisation feature

---
+++ api/InventarizationDemo.Api/Controllers/InventarizationPositionController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class InventarizationPositionController : BaseController
+{
+    [HttpPost("create")]
+    public async Task<ActionResult<InventarizationPosition>> Create(InventarizationPosition createRequest)
+    {
+        return await Mediator.Send(new CreateInventarizationPositionCommand(createRequest));
+    }
+}
---
+++ api/InventarizationDemo.Application/Features/InventarizationPositions/Commands/CreateInventarizationPosition/CreateInventarizationPositionHandler.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+public sealed class CreateInventarizationPositionHandler : ICommandHandler<CreateInventarizationPositionCommand, InventarizationPosition>
+{
+    private readonly ApplicationDbContext applicationDbContext;
+
+    public CreateInventarizationPositionHandler(ApplicationDbContext applicationDbContext)
+    {
+        this.applicationDbContext = applicationDbContext;
+    }
+    public async Task<InventarizationPosition> Handle(CreateInventarizationPositionCommand request, CancellationToken cancellationToken)
+    {
+        EntityEntry<InventarizationPosition> result = await applicationDbContext.InventarizationPositions.AddAsync(request.InventarizationPosition, cancellationToken);
+        await applicationDbContext.SaveChangesAsync(cancellationToken);
+        return result.Entity;
+    }
+}
